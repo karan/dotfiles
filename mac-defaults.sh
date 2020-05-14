3ba84e34a2cab29afb9e6d694f31ef751d627395
Karan Goel <github@goel.io>
2020-05-13 19:52:33 -0700

    mac column view

--- mac-defaults.sh
+++ mac-defaults.sh
@@ -12,7 +12,7 @@ if test "$(uname)" = "Darwin"; then
     defaults write -g ApplePressAndHoldEnabled -bool false
 
     # Always open everything in Finder's list view. This is important.
-    defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+    defaults write com.apple.Finder FXPreferredViewStyle clmv
 
     # Show the ~/Library folder.
     chflags nohidden ~/Library
